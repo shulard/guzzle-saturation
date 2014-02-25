4450310077e91a4c94aff0e688515d721bce2582
Stéphane HULARD <s.hulard@chstudio.fr>
2014-02-25 11:09:06 +0100

    Ajout de nouveaux tests avec bee4 et réutilisation guzzle

--- test.php
+++ test.php
@@ -3,20 +3,33 @@
 require "vendor/autoload.php";
 
 $url = "http://127.0.0.1:9200";
-if( $argc > 1 )
-	$url = $argv[1];
+$limit = 50000;
+if( $argc > 1 ) {
+	$args = [];
+	array_shift($argv);
+	foreach ($argv as $arg) {
+	  if (preg_match("/^--([^=]+)=(.*)$/",$arg,$reg))
+	    $args[$reg[1]] = $reg[2];
+	  elseif(preg_match("/^--([a-zA-Z0-9]+)$/",$arg,$reg))
+	  	$args[$reg[1]] = true;
+	  elseif(preg_match("/^-([a-zA-Z0-9])$/",$arg,$reg))
+	    $args[$reg[1]] = true;
+	}
+	$url = isset($args['url'])?$args['url']:$url;
+	$limit = isset($args['limit'])?$args['limit']:$limit;
+}
 
 echo "------------------------------------------------------------------------".PHP_EOL;
 echo "HTTP Request tester".PHP_EOL;
-echo "Perform 50000 requests on $url and check performances...".PHP_EOL;
+echo "Perform $limit requests on $url and check performances...".PHP_EOL;
 
 echo "------------------------------------------------------------------------".PHP_EOL;
-echo "Try Guzzle calls: ".PHP_EOL;
+echo "Try Guzzle client creation for each call: ".PHP_EOL;
 $start = microtime(true);
 try {
 	$i = 0;
 	$memory = -1;
-	while( $i < 50000 ) {
+	while( $i < $limit ) {
 		$client = new Guzzle\Http\Client();
 		$client->get($url)->send();
 		$i++;
@@ -24,6 +37,7 @@ try {
 		$tmp = memory_get_usage(true);
 		$memory = $memory>$tmp?$memory:$tmp;
 	}
+	echo "OK - Ended".PHP_EOL;
 } catch( \Exception $error ) {
 	echo "  ERROR: ".$error->getMessage().PHP_EOL;
 	echo "  End at: ".$i.PHP_EOL;
@@ -31,13 +45,112 @@ try {
 echo "Duration: ".number_format(microtime(true) - $start, 2)." sec".PHP_EOL;
 echo "Memory: ".number_format(ceil($memory/1000)/1000, 2, '.', ' ')." Mo".PHP_EOL;
 
+sleep(20);
+
+echo "------------------------------------------------------------------------".PHP_EOL;
+echo "Try Guzzle client reuse in all calls: ".PHP_EOL;
+$start = microtime(true);
+try {
+	$i = 0;
+	$memory = -1;
+	$client = new Guzzle\Http\Client();
+	while( $i < $limit ) {
+		$client->get($url)->send();
+		$i++;
+
+		$tmp = memory_get_usage(true);
+		$memory = $memory>$tmp?$memory:$tmp;
+	}
+	echo "OK - Ended".PHP_EOL;
+} catch( \Exception $error ) {
+	echo "  ERROR: ".$error->getMessage().PHP_EOL;
+	echo "  End at: ".$i.PHP_EOL;
+}
+echo "Duration: ".number_format(microtime(true) - $start, 2)." sec".PHP_EOL;
+echo "Memory: ".number_format(ceil($memory/1000)/1000, 2, '.', ' ')." Mo".PHP_EOL;
+
+sleep(20);
+
+echo "------------------------------------------------------------------------".PHP_EOL;
+echo "Try Guzzle request reuse in all calls: ".PHP_EOL;
+$start = microtime(true);
+try {
+	$i = 0;
+	$memory = -1;
+	$client = new Guzzle\Http\Client();
+	$request = $client->get($url);
+	while( $i < $limit ) {
+		$request->send();
+		$i++;
+
+		$tmp = memory_get_usage(true);
+		$memory = $memory>$tmp?$memory:$tmp;
+	}
+	echo "OK - Ended".PHP_EOL;
+} catch( \Exception $error ) {
+	echo "  ERROR: ".$error->getMessage().PHP_EOL;
+	echo "  End at: ".$i.PHP_EOL;
+}
+echo "Duration: ".number_format(microtime(true) - $start, 2)." sec".PHP_EOL;
+echo "Memory: ".number_format(ceil($memory/1000)/1000, 2, '.', ' ')." Mo".PHP_EOL;
+
+sleep(20);
+
+echo "------------------------------------------------------------------------".PHP_EOL;
+echo "Try Bee4 HTTP Client with a new client for each call: ".PHP_EOL;
+$start = microtime(true);
+try {
+	$i = 0;
+	$memory = -1;
+	while( $i < $limit ) {
+		$client = new Bee4\Http\Client();
+		$client->get($url)->send();
+		$i++;
+
+		$tmp = memory_get_usage(true);
+		$memory = $memory>$tmp?$memory:$tmp;
+	}
+	echo "OK - Ended".PHP_EOL;
+} catch( \Exception $error ) {
+	echo "  ERROR: ".$error->getMessage().PHP_EOL;
+	echo "  End at: ".$i.PHP_EOL;
+}
+echo "Duration: ".number_format(microtime(true) - $start, 2)." sec".PHP_EOL;
+echo "Memory: ".number_format(ceil($memory/1000)/1000, 2, '.', ' ')." Mo".PHP_EOL;
+
+sleep(20);
+
+echo "------------------------------------------------------------------------".PHP_EOL;
+echo "Try Bee4 HTTP Client with client reuse: ".PHP_EOL;
+$start = microtime(true);
+try {
+	$i = 0;
+	$memory = -1;
+	$client = new Bee4\Http\Client();
+	while( $i < $limit ) {
+		$client->get($url)->send();
+		$i++;
+
+		$tmp = memory_get_usage(true);
+		$memory = $memory>$tmp?$memory:$tmp;
+	}
+	echo "OK - Ended".PHP_EOL;
+} catch( \Exception $error ) {
+	echo "  ERROR: ".$error->getMessage().PHP_EOL;
+	echo "  End at: ".$i.PHP_EOL;
+}
+echo "Duration: ".number_format(microtime(true) - $start, 2)." sec".PHP_EOL;
+echo "Memory: ".number_format(ceil($memory/1000)/1000, 2, '.', ' ')." Mo".PHP_EOL;
+
+sleep(20);
+
 echo "------------------------------------------------------------------------".PHP_EOL;
 echo "Try to curl init at each iteration: ".PHP_EOL;
 $start = microtime(true);
 try {
 	$i = 0;
 	$memory = -1;
-	while( $i < 50000 ) {
+	while( $i < $limit ) {
 		$handle = curl_init();
 		curl_setopt($handle, CURLOPT_URL, $url);
 		curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
@@ -48,12 +161,15 @@ try {
 		$tmp = memory_get_usage(true);
 		$memory = $memory>$tmp?$memory:$tmp;
 	}
+	echo "OK - Ended".PHP_EOL;
 } catch( \Exception $error ) {
 	echo "  ERROR: ".$error->getMessage().PHP_EOL;
 	echo "  End at: ".$i.PHP_EOL;
 }
 echo "Duration: ".number_format(microtime(true) - $start, 2)." sec".PHP_EOL;
 echo "Memory: ".number_format(ceil($memory/1000)/1000, 2, '.', ' ')." Mo".PHP_EOL;
+
+sleep(20);
 
 echo "------------------------------------------------------------------------".PHP_EOL;
 echo "Try to curl init with resource reuse: ".PHP_EOL;
@@ -62,7 +178,7 @@ try {
 	$i = 0;
 	$memory = -1;
 	$handle = curl_init();
-	while( $i < 50000 ) {
+	while( $i < $limit ) {
 		curl_setopt($handle, CURLOPT_URL, $url);
 		curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
 		curl_exec($handle);
@@ -72,6 +188,7 @@ try {
 		$memory = $memory>$tmp?$memory:$tmp;
 	}
 	curl_close($handle);
+	echo "OK - Ended".PHP_EOL;
 } catch( \Exception $error ) {
 	echo "  ERROR: ".$error->getMessage().PHP_EOL;
 	echo "  End at: ".$i.PHP_EOL;
